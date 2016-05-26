a1bc1c7acb000d9ff05d8a1960dc5cd662c73ff2
TheoLeanse <theoleanse@gmail.com>
2016-05-26 17:29:31 +0100

    think about markup

--- main.js
+++ main.js
@@ -1,36 +1,32 @@
 'use strict';
-
-const DB = firebaseManager();
-DB.init().then(data => {
-    document.querySelector('#data').innerHTML = data;
-});
-
 let beats           = [];
+const DB            = firebaseManager();
 const title         = document.querySelector('#title');
+const submit        = document.querySelector('#submit');
 const display       = document.querySelector('#display');
+const counters      = Array.from(document.querySelectorAll('.counter'));
+const refresher     = document.querySelector('#refresh');
+const dataDisplay   = document.querySelector('#data');
 const similarTracks = document.querySelector('#similar');
 
-document.querySelector('#counter').addEventListener('click', clickHandler);
-document.querySelector('#refresh').addEventListener('click', () => beats = []);
-document.querySelector('#submit').addEventListener('click', saveHandler);
+DB.init().then(data => dataDisplay.innerHTML = data);
 
-function clickHandler () {
+submit.addEventListener('click', () => DB.save(title.value, BPM(beats)));
+refresher.addEventListener('click', () => beats = []);
+
+counters.forEach(counter => counter.addEventListener('click', () => {
     let beat = new Date().getTime() / 1000;
     beats.push(beat);
     const bpm = BPM(beats);
     display.innerHTML = bpm;
     similarTracks.innerHTML = getBPMRange(bpm - 5, bpm + 5);
-}
+}));
 
 function BPM (beatlist) {
     const count = beatlist.length - 1;
-    const time = beatlist[count] - beatlist[0];
+    const time  = beatlist[count] - beatlist[0];
     const tempo = count / time * 60;
     return parseFloat(tempo.toFixed(1));
-}
-
-function saveHandler () {
-    DB.save(title.value, BPM(beats));
 }
 
 function getBPMRange (start, end) {
@@ -39,15 +35,14 @@ function getBPMRange (start, end) {
 
 function firebaseManager () {
     const firebaseConfig = {
-        apiKey: "AIzaSyBB6HN_A8AWdm7QaGAaXOMtsWH1cSSZFjs",
-        authDomain: "beatlister.firebaseapp.com",
-        databaseURL: "https://beatlister.firebaseio.com",
+        apiKey:        "AIzaSyBB6HN_A8AWdm7QaGAaXOMtsWH1cSSZFjs",
+        authDomain:    "beatlister.firebaseapp.com",
+        databaseURL:   "https://beatlister.firebaseio.com",
         storageBucket: "beatlister.appspot.com"
     };
     firebase.initializeApp(firebaseConfig);
     const firebaseDB = firebase.database();
     return {
-        //data: undefined,
         save (title, bpm) {
             firebaseDB.ref('/' + title).set(bpm);
         },
